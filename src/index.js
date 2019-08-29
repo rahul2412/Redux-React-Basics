c59cc09463e19d900d7c15907055f0134b6bf143
Rahul Passi <rahulpassi2412@gmail.com>
2019-08-29 11:18:02 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 import store from "./store";
 import { addArticle } from "./action";
 
@@ -8,3 +9,7 @@ window.addArticle = addArticle;
 store.subscribe(() => console.log('Look man, Redux is doing its Magic'))
 // It is fired whenever state changes
 
+function Check(){
+    return <h2>Use console to see the working of the Redux library</h2>
+}
+ReactDOM.render(<Check/>,document.getElementById('root'));
